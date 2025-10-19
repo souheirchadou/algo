8a7a6c4bac7ec493f70460647321fa405017e3c5
souheirchadou <chadousouha@gmail.com>
2025-10-19 13:15:39 +0100

    Add files via upload

---
+++ TP6.py
@@ -0,0 +1,6 @@
+Python 3.2.2 (default, Sep  4 2011, 09:51:08) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+Numbers divisible by 13 are [65, 39, 221]
+>>> 
